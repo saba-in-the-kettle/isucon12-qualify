a5745b6dbb605b30c7a3964e9e62ce80c81543a5
KMConner <KMConner@users.noreply.github.com>
2022-07-23 10:30:14 +0900

    Add deploy script

---
+++ s2/post-deploy.sh
@@ -0,0 +1,26 @@
+set -x
+
+sudo chmod 777 /var/log/nginx
+sudo chmod 777 /var/log/nginx/*
+sudo chmod 777 /var/log/mysql
+sudo chmod 777 /var/log/mysql/*
+
+set -e
+
+truncate -s 0 /var/log/mysql/mysql-slow.log
+truncate -s 0 /var/log/nginx/access.log
+
+sudo systemctl restart nginx
+sudo systemctl restart mysql
+
+QUERY="CREATE USER IF NOT EXISTS 'isucon'@'%' identified by 'isucon';
+GRANT ALL privileges on *.* to isucon@'%';
+ALTER USER isucon@'%' IDENTIFIED WITH mysql_native_password BY 'isucon';
+flush privileges;
+"
+
+# echo "$QUERY" | sudo mysql -uroot
+
+sudo ufw disable
+sudo service apparmor stop
+sudo systemctl disable apparmor 
