f36446d2fecafc7d4c217bad7a1083ddeb1bdd75
p1ass <naoki.kishi@dena.com>
2022-08-08 23:24:17 +0900

    sqliteにインデックスを貼る

--- sql/init.sh
+++ sql/init.sh
@@ -9,13 +9,20 @@ ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 
+# SQLiteのデータベースを初期化
+rm -f ../tenant_db/*.db
+cp -r ../../initial_data/*.db ../tenant_db/
+
+for f in ../tenant_db/*.db;
+do
+  QUERY="create index if not EXISTS competition_index on competition(created_at);
+  create index if not EXISTS player_score_index2 on player_score(player_id, competition_id);"
+  echo "$QUERY" | sqlite3 "$f"
+done
+
 # MySQLを初期化
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < init.sql
-
-# SQLiteのデータベースを初期化
-rm -f ../tenant_db/*.db
-cp -r ../../initial_data/*.db ../tenant_db/
